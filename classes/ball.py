24ee22baf460589b08c36ab0c7c38c92f73a2a76
Miguel Barraza <62113405+mabarraza@users.noreply.github.com>
2023-10-22 17:02:08 -0300

    increase speed added

--- classes/ball.py
+++ classes/ball.py
@@ -6,15 +6,15 @@ class Ball():
         self.radius = 10
         self.x_coordinate = width/2 - self.radius
         self.y_coordinate = height/2 - self.radius
-        self.x_velocity = 1.5
-        self.y_velocity = 1.5
+        self.x_velocity = 1.0
+        self.y_velocity = 1.0
         self.left_side_collision = pygame.event.custom_type()
         self.right_side_collision = pygame.event.custom_type()
 
     def check_collision(self, players_positions: list[list[int]], players_proportions: list[int]):
         has_player_collided = self.check_player_collision(players_positions, players_proportions)
         if not has_player_collided:
-            self.check_wall_collision()
+            has_ball_collided = self.check_wall_collision()
 
     def check_player_collision(self, players_positions: list[list[int]], players_proportions: list[int]):
         players_width = players_proportions[0]
@@ -31,31 +31,45 @@ class Ball():
         if has_left_player_collided:
             self.x_coordinate = left_player_position[0] + players_width
             self.x_velocity *= -1
+            self.increase_speed()
 
         if has_right_player_collided:
             self.x_coordinate = right_player_position[0]
             self.x_velocity *= -1
+            self.increase_speed()
         return has_colided
 
     def check_wall_collision(self):
         has_colided = False
         if self.y_coordinate <= 0 + self.radius or self.y_coordinate >= 600 - self.radius:
-            self.y_velocity *= -1 #si toca el borde superor o inferior de la pantalla cambiar sentido
+            self.y_velocity *= -1.0 #si toca el borde superor o inferior de la pantalla cambiar sentido
             has_colided = True
+            self.increase_speed()
         if self.x_coordinate >= 800 - self.radius: #right side collision
             self.x_coordinate = 800/2 - self.radius
             self.y_coordinate = 600/2 - self.radius
-            self.x_velocity *= -1
-            self.y_velocity *= -1
+            self.x_velocity = -1.0
+            self.y_velocity = 1.0
             has_colided = True
             pygame.event.post(pygame.event.Event(self.right_side_collision))
         if self.x_coordinate <= 0 + self.radius: #left side collision
             self.x_coordinate = 800/2 - self.radius
             self.y_coordinate = 600/2 - self.radius
-            self.x_velocity = 1.5
-            self.y_velocity = 1.5
+            self.x_velocity = 1.0
+            self.y_velocity = 1.0
             has_colided = True
             pygame.event.post(pygame.event.Event(self.left_side_collision))
+        return has_colided
+
+    def increase_speed(self,):
+        if self.x_velocity > 0:
+            self.x_velocity += 0.2
+        else:
+            self.x_velocity += -0.2
+        if self.y_velocity > 0:
+            self.y_velocity += 0.2
+        else:
+            self.y_velocity += -0.2
 
     def move(self):
         self.x_coordinate += self.x_velocity
